32d6d8780716c0dd32697670ec5c322c4a2d4d31
Samuel L. <72248110+Samxel@users.noreply.github.com>
2025-07-30 11:47:55 +0200

    fix: restrict loot modifier to structure chests and barrels only

--- src/main/java/com/samxel/reducedloot/ChanceLootModifier.java
+++ src/main/java/com/samxel/reducedloot/ChanceLootModifier.java
@@ -3,6 +3,7 @@ package com.samxel.reducedloot;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
@@ -18,18 +19,24 @@ public class ChanceLootModifier extends LootModifier {
         super(conditions);
     }
 
-    @NotNull
     @Override
-    protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        ObjectArrayList<ItemStack> filtered = new ObjectArrayList<>();
-        double chance = Config.chance;
-        var rand = context.getRandom();
-        for (ItemStack stack : generatedLoot) {
-            if (rand.nextDouble() < chance) {
-                filtered.add(stack);
+    protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
+        ResourceLocation tableId = context.getQueriedLootTableId();
+
+        String path = tableId.getPath();
+        String namespace = tableId.getNamespace();
+        if (namespace.equals("minecraft") && (path.startsWith("chests/"))) {
+            ObjectArrayList<ItemStack> filtered = new ObjectArrayList<>();
+            double chance = Config.chance;
+            var rand = context.getRandom();
+            for (ItemStack stack : generatedLoot) {
+                if (rand.nextDouble() < chance) {
+                    filtered.add(stack);
+                }
             }
+            return filtered;
         }
-        return filtered;
+        return generatedLoot;
     }
 
     @Override
